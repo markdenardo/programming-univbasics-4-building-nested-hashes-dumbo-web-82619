22d470a35c0b8bd60cc91a33e77dd7f5d56fa0ea
Mark DeNardo <mark.denardo@gmail.com>
2019-08-18 23:19:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
                     {:reading_railroad => {},
                       :pennsylvania_railroad => {},
                       :b_and_o_railroad => {},
-                      :shortline_railroad => $100
+                      :shortline_railroad => {}
                     }
                   }
               }
