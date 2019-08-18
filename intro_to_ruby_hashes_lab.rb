63ad7c6f9eac58857042411618a2082ae3dcf579
Mark DeNardo <mark.denardo@gmail.com>
2019-08-18 23:16:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,10 +21,10 @@ def monopoly_with_third_tier
                 :railroads => 
                   {:pieces => 4,
                     :rent_in_dollars => 
-                                {:one_piece_owned => 25,
-                                :two_pieces_owned => 50,
-                                :property3 => 300,
-                                :property4 => 400},
+                                {:one_piece_owned=>25,
+                                :two_pieces_owned=>50,
+                                :three_pieces_owned=>100,
+                                :four_pieces_owned=>200},
                   
                 :names => 
                     {:name1 => {},
